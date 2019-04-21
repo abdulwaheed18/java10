8c555adf70c1abce5408864f409a68f5b032dada
ABdul Waheed <abdulwaheed18@gmail.com>
2019-04-21 20:47:39 +0530

    JAVA 10 - Initial Commit
    Added var feature

---
+++ src/com/waheedtechblog/typeinference/TypeInferenceExample.java
@@ -0,0 +1,56 @@
+package com.waheedtechblog.typeinference;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class TypeInferenceExample {
+
+	static {
+		// static block variable
+		var staticVaribale = "var in static block";
+		Consumer<String> displayOnConsole = msg -> {
+			System.out.println(msg);
+		};
+		displayOnConsole.accept(staticVaribale);
+
+	}
+
+	public static void main(String[] args) {
+
+		TypeInferenceExample inferenceExample = new TypeInferenceExample();
+
+		BiConsumer<String, String> displayOnConsole = (str, msg) -> {
+			System.out.println(str + msg);
+		};
+
+		// Before Java 10
+		String message = "Welcome to Waheedtechblog.com";
+		displayOnConsole.accept("Before JDK 10: ", message);
+
+		// Using JDK 10, Local Variable
+		var message10 = "Welcome to Waheedtechblog.com";
+		displayOnConsole.accept("With JDK 10: ", message);
+
+		// local variable declaration in enhanced loops
+		String[] countryName = { "India", "Japan", "UAE", "USA", "UK" };
+		displayOnConsole.accept("", "List of Country using enhanced loop");
+		for (var country : countryName) {
+			displayOnConsole.accept("", country);
+		}
+
+		// basic for loop
+		displayOnConsole.accept("", "List of Country using basic loop");
+		for (var i = 0; i < countryName.length; i++) {
+			displayOnConsole.accept("", countryName[i]);
+		}
+
+		displayOnConsole.accept("Length of string '" + message10 + "' is: ",
+				String.valueOf(inferenceExample.getLenth(message10)));
+	}
+
+	private int getLenth(String msg) {
+		var length = msg.length();
+		return length;
+	}
+
+}
